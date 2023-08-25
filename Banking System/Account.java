b00217c803829dc76f7e6174875284bff96f4581
AkshitGoel247 <137819251+AkshitGoel247@users.noreply.github.com>
2023-08-25 20:47:44 +0530

    Update Account.java

--- Banking System/Account.java
+++ Banking System/Account.java
@@ -9,7 +9,7 @@ public class Account {
     // default constructor
     public Account(){}
 
-
+    // constructor which takes parameters namely username, accountnumber, pin number, accountbalance
     public Account(String userName, int accountNumber, int pinNumber, double accountBalance){
         this.userName = userName;
         this.accountNumber = accountNumber;
@@ -17,6 +17,7 @@ public class Account {
         this.accountBalance = accountBalance;
     }
 
+    // constructor which takes only 2 parameters named username and pinnumber
     public Account(String userName, int pinNumber){
         this.userName = userName;
         this.pinNumber = pinNumber;
@@ -27,61 +28,3 @@ public class Account {
 
 
 
-
-
-
-
-//public class Account {
-//
-//    // username
-//    private String userName;
-//
-//    // account number
-//    private int accountNumber;
-//
-//    // balance in account
-//    private double accountBalance;
-//
-//    public Account(){
-//        // default constructor
-//    }
-//
-//    // constructor
-//    public Account(int accountNumber, String userName){
-//        this.accountNumber = accountNumber;
-//        this.accountBalance = 0.00;
-//        this.userName = userName;
-//    }
-//
-//    // method for deposit
-//    public void depositMoney(double depositAmount){
-//        accountBalance = accountBalance + depositAmount;
-//    }
-//
-//    // method for withdraw
-//    public void withdrawMoney(double amountWithdraw){
-//        if(amountWithdraw > accountBalance){
-//            System.out.println("Insufficient Balance");
-//        }
-//        else{
-//            accountBalance = accountBalance - amountWithdraw;
-//        }
-//    }
-//
-//    // method to get account number
-//    public int getAccountNumber(){
-//        return accountNumber;
-//    }
-//
-//    // method to get account's username
-//    public String getUserName(){
-//        return userName;
-//    }
-//
-//    // method to get account balance
-//    public double getAccountBalance(){
-//        return accountBalance;
-//    }
-//
-//
-//}
